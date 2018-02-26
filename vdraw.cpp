b466f1967700b80dee32eadc49a39e378d35ed18
Aman Jain <aj161198@gmail.com>
2018-02-26 17:14:14 +0530

    Update vdraw.cpp

--- vdraw.cpp
+++ vdraw.cpp
@@ -17,43 +17,43 @@ int main()
 		cout << "Error in using VideoCapture function." << endl;	
 		return -1;
 	}
+	namedWindow("Original",CV_WINDOW_AUTOSIZE);
 	namedWindow("Drawing",CV_WINDOW_AUTOSIZE);
+
+	Point2i initial, final_;
+	int low_r = 0, low_b = 54, low_g = 0, high_r = 67, high_b = 173, high_g = 59;	//Initializes the track-bar values 	
+	int count = 0, flag = 0;
 	
-	int low_r = 0, low_b = 168, low_g = 0, high_r = 255, high_b = 255, high_g = 152;	//Initializes the track-bar values 	
-	
-	/*
 	createTrackbar("Low_Red", "Original", &low_r, 255);			// Create Track-Bar
 	createTrackbar("Low_Blue", "Original", &low_b, 255);		
 	createTrackbar("Low_Green", "Original", &low_g, 255);
 	createTrackbar("High_Red", "Original", &high_r, 255);
 	createTrackbar("High_Blue", "Original", &high_b, 255);
-	createTrackbar("High_Green", "Original", &high_g, 255);*/
+	createTrackbar("High_Green", "Original", &high_g, 255);
 	
 	int R = 0, G = 0, B = 0, thick = 3;
 	createTrackbar("RED", "Drawing", &R, 255);			// Create Track-Bar
 	createTrackbar("BLUE", "Drawing", &B, 255);		
 	createTrackbar("GREEN", "Drawing", &G, 255);
 	createTrackbar("Thickness", "Drawing", &thick, 10);
-
 	Mat canvas = Mat::zeros(480, 640, CV_8UC3);
 	while(1)
     	{
 		Mat frame;
+		count++;
 		bool cap_status = cap.read(frame); // get a new frame from camera
 		if (cap_status == 0)			//If status is false returns 1 to console
 		{
 			cout << "Error in capturing Frames" << endl;			
 			return 1;
 		}
-
-		
 			
-		/*setTrackbarPos("Low_Red","Original", low_r);			// Set the values of Trackbar in runtime to threshold.
+		setTrackbarPos("Low_Red","Original", low_r);			// Set the values of Trackbar in runtime to threshold.
 		setTrackbarPos("Low_Blue","Original", low_b);
 		setTrackbarPos("Low_Green","Original", low_g);
 		setTrackbarPos("High_Red","Original", high_r);
 		setTrackbarPos("High_Blue","Original", high_b);
-		setTrackbarPos("High_Green","Original", high_g);*/	
+		setTrackbarPos("High_Green","Original", high_g);	
 //<--------------------------------------------------------------------------------------------------------------------------->//
 		Mat thresh;			
 		inRange(frame,Scalar(low_b,low_g,low_r), Scalar(high_b,high_g,high_r),thresh);	//Threshold using inRange Function
@@ -65,7 +65,7 @@ int main()
 		dilate(thresh, thresh, element);                                       
 		
 		
-		//imshow("Thresholding", thresh);		//Shows the thresholded frames
+		imshow("Thresholding", thresh);		//Shows the thresholded frames
 //<----------------------------------------------------------------------------------------------------------------------------------->//
 
 		setTrackbarPos("RED","Drawing", R);			// Set the values of Trackbar in runtime to threshold.
@@ -77,58 +77,52 @@ int main()
 		vector<vector<Point> > contours;	
 		vector<Vec4i> hierarchy;
 
-		findContours(thresh_c, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);		//Find contours and store it in contours
+		findContours(thresh_c, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);		//Find contours and store it in contours
 		Mat img(frame.size(), CV_8UC3, Scalar(0, 0, 0));						//Single channel black image is created
 		
 		// Filtering of Contours
 				
-		Point2f corners[4];
 		
 		double area = -1;
-		size_t index = -1;
-		for (size_t idx = 0; idx < contours.size(); idx++) 					//Locating all the corners for every contour and drawing the rectangles above some value
+		int index = -1;
+		for (int idx = 0; idx < contours.size(); idx++) 					
         	{
-			if (contourArea(contours[idx]) > 2000)							// To remove noises sets threshold value as 5000
+			if (contourArea(contours[idx]) > 600)							// To remove noises sets threshold value as 5000
 			{
-				double epsilon = arcLength(contours[idx], true) * 0.1; 				// calculate error value based on arclength			
-				approxPolyDP(contours[idx], contours[idx], epsilon, true);			// approximates any contour to a polygon				
-				if (contours[idx].size() == 4) 							// If polygon has 4 sides
-				{
-					if (area < contourArea(contours[idx]))
-					{	
-						area = contourArea(contours[idx]);
-						index = idx;
-					}
+				if (area < contourArea(contours[idx]))
+				{	
+					area = contourArea(contours[idx]);
+					index = idx;
 				}
 			}
 		}
-
-		if (index >= 0 && area > 2000)
+		cout << (int)index << endl;
+		if (index >= 0 )
 		{
-			drawContours(frame, contours, index, Scalar(255, 0, 255), 1, 8, hierarchy);		// Draws the contours
-			corners[0] = contours[index][0];						// And Stores all the corner values
-			corners[1] = contours[index][1];
-			corners[2] = contours[index][2];
-			corners[3] = contours[index][3];		
-		}
-		
-		int cx = int((corners[0].x + corners[1].x + corners[2].x + corners[3].x) / 4);
-    		int cy = int((corners[0].y + corners[1].y + corners[2].y + corners[3].y) / 4);	 
-		
-		Vec3b color;
-		color[0] = B;
-		color[1] = G;
-		color[2] = R;
-		
-		for (int i = 0; i < thick; i++)
-		{
-			for (int j = 0; j < thick; j++)
+			
+			drawContours(frame, contours, index, Scalar(255,0,0), CV_FILLED, 8);			
+			Moments M = moments(contours[index]); 	
+			int cx = int(M.m10 / M.m00);
+    			int cy = int(M.m01 / M.m00);
+			circle(frame, Point(cx, cy), 3, Scalar(0, 255, 0), CV_FILLED, 8);
+			if (count == 1 || flag == 1)
 			{
-				canvas.at<Vec3b>(Point(cx + j, cy + i)) = color;
+				initial = Point(cx, cy);
+				final_ = Point(cx, cy);
+				flag = 0;
 			}
+			else
+			{
+				initial = final_;
+				final_ = Point(cx, cy);
+			}
+			line(canvas, initial, final_, Scalar(B, G, R), thick, 8, 0);		
+			//count = count % 10000;
 		}
-	
-		//cout << color << endl;
+		else if (index == -1)
+		{
+			flag = 1;
+		}
 		imshow("Drawing", canvas);
 		imshow("Original", frame);
 		if (waitKey(1) == 27)
